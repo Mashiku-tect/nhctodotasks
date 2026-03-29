8cc265b615e7f87c135681a5f789d6b7c84bbadf
nshobairwe <witnes.nshobairwe.gmail.com>
2026-03-29 14:01:28 +0300

    this is sunday

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,7 @@ mkdir -p /app/data /app/media /app/staticfiles
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-exec gunicorn nhctodo.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn nhctodo.wsgi:application \
+    --bind 0.0.0.0:${PORT:-8000} \
+    --workers ${GUNICORN_WORKERS:-3} \
+    --timeout ${GUNICORN_TIMEOUT:-120}
